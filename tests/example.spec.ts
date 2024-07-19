1e1c76b96b73b3c32508ea10d3dee0e3f1a86df0
pawelcz44 <pawel161@gmail.com>
2024-07-19 14:05:28 +0200

    message

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,3 +16,10 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+test('TTMS website', async ({ page }) => {
+  await page.goto('https://ttms.com/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/TTMS/);
+});
