0ef8a726448572afebe011670637f34d72234e82
Miranishi <114513030+Miranishi@users.noreply.github.com>
2024-07-21 12:45:13 +0200

    Test Update - Adding Test against TTMS page
    Editing the test to add one additional test against TTMS page to check whether it has TTMS in it's Title property

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -7,6 +7,13 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
 });
 
+test('has title TTMS', async ({ page }) => {
+  await page.goto('https://ttms.com/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/TTMS/);
+});
+
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
