09fcb82985767a3f69d42f987bfe4a49e007a504
jkrawczyktst <jaroslawkrawczyktst@gmail.com>
2024-07-25 08:12:47 +0200

    Delete ttms1.spec.ts

--- tests/ttms1.spec.ts
+++
@@ -1,6 +0,0 @@
-test('fill in TTMS contact form', async ({ page }) => {
-  await page.goto('https://ttms.com//');
-
-  // Open Contact us form
-  await page.getByRole('link', { name: 'Contact us' }).click();
-});
