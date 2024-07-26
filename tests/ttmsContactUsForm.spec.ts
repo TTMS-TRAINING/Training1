df6dba7cac0f3de1bc79d834758d24cd2ace804a
jkrawczyktst <jaroslawkrawczyktst@gmail.com>
2024-07-26 08:42:25 +0200

    Create ttmsContactUsForm.spec.ts

---
+++ tests/ttmsContactUsForm.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://ttms.com/');
+
+  await page.getByTestId('uc-accept-all-button').click();
+  // Open Contact us form
+  await page.locator('#menu-item-2546').getByRole('link', { name: 'Contact' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+    await page.getByLabel('Name *', { exact: true }).fill('Jarek');
+    await page.waitForTimeout(2000);
+    await page.getByLabel('Surname *').fill('Krawczyk');
+    await page.waitForTimeout(2000);
+    await page.getByLabel('Phone').fill('1234567890');
+    await page.waitForTimeout(2000);
+    await page.getByLabel('E-mail *').fill('test@test.test');
+    await page.waitForTimeout(2000);
+    await page.getByLabel('Message').fill('Pick locator rulez');
+    // await page.waitForTimeout(30000);
+}); 
