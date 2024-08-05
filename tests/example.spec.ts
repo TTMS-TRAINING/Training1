8b01f8cd52f6eeccf0760c796a72a46186106323
jkrawczyktst <jaroslawkrawczyktst@gmail.com>
2024-08-05 16:56:39 +0200

    upadate

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -13,6 +13,6 @@ test('get started link', async ({ page }) => {
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects page to have a heading with the name of Installation.
+  // Expects page to have a heading with the name of Installation..
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
