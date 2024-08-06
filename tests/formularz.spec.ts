959c80cb593440d7d4074d938226494cd12350bd
kamilakrystyna <51360025+kamilakrystyna@users.noreply.github.com>
2024-08-06 14:21:14 +0200

    Delete not used import

--- tests/formularz.spec.ts
+++ tests/formularz.spec.ts
@@ -1,5 +1,4 @@
 import { test, expect} from '@playwright/test';
-import { TIMEOUT } from 'dns';
 
 test('test', async ({ page }) => {
       await page.goto('https://demoqa.com/automation-practice-form/');
