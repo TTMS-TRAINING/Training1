d641b6338b3aa5817123babaccf7bf2ef491d75f
pawelcz44 <pawel161@gmail.com>
2024-08-12 15:36:42 +0200

    dodatki

--- tests/e2e/tests/headerPage.spec.ts
+++ tests/e2e/tests/headerPage.spec.ts
@@ -1,5 +1,4 @@
 import { test, expect } from '@playwright/test';
-import { BasePage } from '../pages/basePage';
 import { HeaderPage } from '../pages/headerPage';
 
 test.describe('TTMS Header Page', () => {
@@ -7,43 +6,50 @@ test.describe('TTMS Header Page', () => {
 
     // Setup przed każdym testem
     test.beforeEach(async ({ page }) => {
-        headerPage = new HeaderPage(page); // Inicjalizacja obiektu ContactPage
-        await headerPage.navigate('https://ttms.com'); // Nawigacja do strony 
-        await headerPage.acceptCookiesAll();
+        headerPage = new HeaderPage(page); // Inicjalizacja obiektu HeaderPage
+        await headerPage.navigate('https://ttms.com'); // Nawigacja do strony TTMS
+        await headerPage.acceptCookiesAll(); // Akceptacja cookies
     });
 
-    test('Check About us element on Header', async ({ page }) => {
-
-        await headerPage.aboutUS();
-
+    // Test sprawdzający menu "About Us" na nagłówku
+    test('Check About Us element on Header', async ({ page }) => {
+        await headerPage.aboutUS(); // Weryfikacja menu "About Us"
     });
 
+    // Test weryfikujący linki submenu "About Us"
     test('Verify submenu links from About Us', async ({ page }) => {
-
-        await headerPage.clickAndVerifySubmenuLink1();
-        await headerPage.clickAndVerifySubmenuLink2();
-        await headerPage.clickAndVerifySubmenuLink3();
-
-
+        await headerPage.clickAndVerifySubmenuLink1(); // Weryfikacja linku "Who we are"
+        await headerPage.clickAndVerifySubmenuLink2(); // Weryfikacja linku "Pressroom"
+        await headerPage.clickAndVerifySubmenuLink3(); // Weryfikacja linku "Sustainable TTMS"
     });
 
+    // Test weryfikujący działanie logo TTMS
     test('Verify TTMS Logo', async ({ page }) => {
-
-        await headerPage.clickAndVerifyLogo();
-
+        await headerPage.clickAndVerifyLogo(); // Weryfikacja przekierowania po kliknięciu logo TTMS
     });
 
+    // Test weryfikujący menu "Specializations"
     test('Verify Specializations menu item', async ({ page }) => {
-
-        await headerPage.Specializations();
-
+        await headerPage.Specializations(); // Sprawdzenie, czy menu "Specializations" jest poprawne
     });
 
+    // Test weryfikujący menu "Industries"
     test('Verify Industries menu item', async ({ page }) => {
-        await headerPage.Industries();
-
+        await headerPage.Industries(); // Sprawdzenie, czy menu "Industries" jest poprawne
     });
 
+    // Test weryfikujący menu "Case Studies"
+    test('Verify Case Studies menu item', async ({ page }) => {
+        await headerPage.CaseStudies(); // Sprawdzenie, czy menu "Case Studies" jest poprawne
+    });
 
+    // Test weryfikujący menu "Blog"
+    test('Verify Blog menu item', async ({ page }) => {
+        await headerPage.Blog(); // Sprawdzenie, czy menu "Blog" jest poprawne
+    });
 
+    // Test weryfikujący menu "Career"
+    test('Verify Career menu item', async ({ page }) => {
+        await headerPage.Career(); // Sprawdzenie, czy menu "Career" jest poprawne
+    });
 });
