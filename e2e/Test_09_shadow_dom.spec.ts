8941f0fe65d220e8600bd605d837cc28129e68de
Filip Matyaszewski <filip.matyaszewski@gmail.com>
2024-08-19 17:22:44 +0200

    Shadow DOM
    webpage is not working 100% so a full test is not possible

---
+++ e2e/Test_09_shadow_dom.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+
+test('Pole user name', async ({ page }) => {
+    await page.goto('https://selectorshub.com/xpath-practice-page/');
+    await page.waitForSelector('div#loader', { state: 'hidden' });
+    await page.getByPlaceholder('enter name', { exact: true }).fill('Pizza Eater');
+});
+test('Pole pizza name', async ({ page }) => {
+    await page.goto('https://selectorshub.com/xpath-practice-page/');
+    await page.waitForSelector('div#loader', { state: 'hidden' });
+    await page.getByPlaceholder('enter name', { exact: true }).fill('Monster Pizza');
+    });
+test('Pole Destiny', async ({ page }) => {
+    test.setTimeout(120000);
+
+    await page.goto('https://selectorshub.com/xpath-practice-page/');
+    await page.waitForSelector('div#loader', { state: 'hidden' });
+    await page.getByRole('link', { name: 'Click to practice iframe' }).click();
+    await page.keyboard.press('Enter');
+    await page.frameLocator('#pact1').getByPlaceholder('Destiny').fill('Pizza is my destiny!');
+
+});
