5c1ec321ee6aeb24752787d4927360da8184b5d7
Ernest Jurek <ernest.jurek@ttms.pl>
2024-09-25 18:54:40 +0200

    Zadanie 7

---
+++ data/TestData.ts
@@ -0,0 +1,11 @@
+export const homePageTestData = {
+  expectedTitle: "AI-powered IT systems at your fingertips. We are trusted and modern IT partner.",
+};
+
+export const contactPageTestData= {
+  name: "AAAA",
+  surname: "BBBB",
+  email: "test@example.com",
+  phone: "123456789",
+  message: "This is a test message.",
+};
---
+++ pages/basePage.ts
@@ -0,0 +1,22 @@
+import { Page, Locator } from '@playwright/test';
+
+export class BasePage {
+    protected page: Page;
+
+    constructor(page: Page) {
+        this.page = page;
+    }
+    async navigateTo(url: string) {
+        await this.page.goto(url);
+    }
+
+    async acceptCookies() {
+        const acceptCookiesButton: Locator = this.page.locator('#onetrust-accept-btn-handler');
+        if (await acceptCookiesButton.isVisible()) {
+            await acceptCookiesButton.click();
+        }
+    }
+    async getPageTitle(): Promise<string> {
+        return this.page.title();
+    }
+}
